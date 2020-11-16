ac1d28148a09caf54311004e8f380051c2e11577
KOSMOGOR <mortalmortal@mail.ru>
2020-11-16 17:48:28 +0300

    First commit

--- first.py
+++ first.py
@@ -1,9 +1,1 @@
-def main():
-    print('My first git program')
-    print('New thing!')
-    print('Anothe thing')
-    print('New thing, but from GitHub!')
-
-
-if __name__ == "__main__":
-    main()
+print('My first git repo')
